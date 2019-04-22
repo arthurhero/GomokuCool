7356bb2a0dea8b94be456724cdd8a4356ad3296c
Chen <chenziwe@ip-132-161-196-150>
2019-04-22 15:48:51 -0500

    added sth in gomoku

--- info.h
+++ info.h
@@ -2,6 +2,7 @@
 #define HOST_WIN 1
 #define GUEST_WIN 2
 #define DRAW 4
+#define WAITING 5 //waiting for an opponent to connect
 
 #define BLANK 0
 #define HOST 1
@@ -11,13 +12,43 @@
 #define BOARD_HEIGHT 2
 #define BOARD_WIDTH 4
 
-// Defines used to track the piece direction
-#define DIR_NORTH 0
-#define DIR_EAST 1
-#define DIR_SOUTH 2
-#define DIR_WEST 3
+// Defines user inputs 
+#define UP 0
+#define RIGHT 1
+#define DOWN 2
+#define LEFT 3
+#define ENTER 4
+#define QUIT 5
+#define YES 6
+#define NO 7
 
-// Game parameters
-#define DRAW_BOARD_INTERVAL 250
-#define READ_OPPONENT_INTERVAL 150
-#define READ_INPUT_INTERVAL 150 //arrow key and enter key and quit key
+// structs for task methods
+typedef struct game_stat {
+    bool *host;
+    int *status; //running or host win or guest win or draw
+    int *cur_c;
+    int *cur_r;
+    int **board;
+} game_stat_s;
+
+// struct for read input
+typedef struct user_input {
+    int *status; //our status
+    int *input;
+} user_input_s;
+
+// structs for opponent inputs
+typedef struct op_info {
+    int *status; //our status
+    int *r;
+    int *c;
+    int *op_status;
+    bool *offline;
+} op_info_s;
+
+// structs for send inputs
+typedef struct send_info {
+    int r;
+    int c;
+    int status;
+} send_info_s;
